e31dcd498d5d0d1e1fc8a62ebd1793f3b91f0a08
sevin98 <psv980817@naver.com>
2026-02-20 10:48:59 +0900

    test: 테스트 디렉토리 추가

---
+++ src/test/java/com/prography11thbackend/Prography11thBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prography11thbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prography11thBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
