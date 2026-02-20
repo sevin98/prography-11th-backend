c55d05c6ea124ae26213a3841084b65a12e4269a
sevin98 <psv980817@naver.com>
2026-02-20 10:48:36 +0900

    feat: Spring Boot 애플리케이션 메인 클래스

---
+++ src/main/java/com/prography11thbackend/Prography11thBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prography11thbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prography11thBackendApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(Prography11thBackendApplication.class, args);
+    }
+
+}
